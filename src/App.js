39dfaf9de38bf798458e713b036cd0d5f234c2c5
shashank dewangan <shashank.at30@gmail.com>
2021-05-28 17:36:43 +0530

    dialog changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { DialogBox } from './components/DialogBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    open: false,
+  };
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.resetTimer);
+    document.addEventListener('keypress', this.resetTimer);
+    this.timer();
+  }
+
+  timer = () => setTimeout(() => this.setState({ open: true }), 3000);
+
+  resetTimer = () => {
+    clearTimeout(this.timer);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>this is main page</h1>
+        <div className="dialog">
+          {this.state.open && (
+            <DialogBox onOk={() => this.setState({ open: false })} />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DialogBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import './style.css';
+
+export const DialogBox = ({ onOk, onCancel }) => {
+  return (
+    <div className="dialog-box">
+      <Container>
+        <Row>
+          <Col xs={12} md={6}></Col>
+          Click OK to continue else go to login
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <Button type="button" variant="light" onClick={onOk}>
+              Ok
+            </Button>
+            <Button type="button" variant="light" onClick={onCancel}>
+              Cancel
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
